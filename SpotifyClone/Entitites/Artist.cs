075a3001d7b2e830cc93fbeca35d2597881f8357
simplyana08 <ana.t08@yahoo.com>
2026-05-29 11:19:59 +0200

    Artist name gegeven

---
+++ SpotifyClone/Entitites/Artist.cs
@@ -0,0 +1,12 @@
+namespace SpotifyClone.Entities
+{
+    public class Artist
+    {
+        public string Name { get; private set; }
+
+        public Artist(string name)
+        {
+            Name = name;
+        }
+    }
+}
