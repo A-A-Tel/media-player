5684e4f07362a3e79485aae15a6e3ff753e39759
simplyana08 <ana.t08@yahoo.com>
2026-06-02 11:10:00 +0200

    Artists class

--- SpotifyClone/Entitites/Artist.cs
+++ SpotifyClone/Entitites/Artist.cs
@@ -6,7 +6,10 @@ namespace SpotifyClone.Entities
 
         public Artist(string name)
         {
-            Name = name;
-        }
+            public string Name { get; private set; }
+        public List <Album> Albums { get; private set; } = new List<Album>();
+        public List <Song> Songs { get; private set; } = new List<Song>();
+       
+    }
     }
 }
