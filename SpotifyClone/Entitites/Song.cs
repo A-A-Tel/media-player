82042b369b6bc3bb895bea457e203566c12cbb1c
simplyana08 <ana.t08@yahoo.com>
2026-06-02 11:06:34 +0200

    Song class

--- SpotifyClone/Entitites/Song.cs
+++ SpotifyClone/Entitites/Song.cs
@@ -2,7 +2,20 @@ namespace SpotifyClone.Entities
 {
     public class Song
     {
-        public string Title { get; private set; }
+        public string Title { get; private set; };
+        public Artist artist { get; private set; };
+        public double duration { get; private set; };
+        pubic string genre { get; private set; };
+        public string releaseDate { get; private set; };
+
+        public Song(string title, Artist artist, double duration, string genre, string releaseDate)
+        {
+            Title = title;
+            this.artist = artist;
+            this.duration = duration;
+            this.genre = genre;
+            this.releaseDate = releaseDate;
+        }
 
         public Song(string title)
         {
